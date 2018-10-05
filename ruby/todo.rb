3cee57950eeb316569fa3a6b589b53b723b5c758
David Jungst <djungst@gmail.com>
2018-10-04 18:56:01 -0600

    structure

--- ruby/todo.rb
+++ ruby/todo.rb
@@ -3,12 +3,54 @@ require 'colorize'
 
 @items = []
 @cont = true
-@options = ['Show All', 'Show Complete', 'Show Active', 'Add Item', 'Mark As Complete', 'Delete Item', 'Exit']
+@options = ['Show All', 'Show Complete', 'Show Active', 'Add Item', 'Mark As Complete', 'Delete Item', 'Quit']
 
 def menu
   puts "====== TODO Ruby =====".colorize(:cyan)
   @options.each_with_index { |item, index| puts "#{index + 1}) #{item}" }
   puts "======================".colorize(:cyan)
+  user_choice
+end
+
+def user_choice
+  puts "Please make a selection: (1 - #{@options.length})\n"
+  choice = gets.to_i
+  if choice > 0 && choice <= @options.length
+    selection = @options[choice - 1].gsub(" ", "_").downcase.to_sym
+    send selection
+  else
+    puts "Invalid choice".colorize(:red)
+    user_choice
+  end
+end
+
+def show_all
+  puts 'show all'
+end
+
+def show_complete
+  puts 'show complete'
+end
+
+def show_active
+  puts 'show active'
+end
+
+def add_item
+  puts 'add item'
+end
+
+def mark_as_complete
+  puts 'mark as complete'
+end
+
+def delete_item
+  puts 'delete item'
+end
+
+def quit
+  puts 'quit'
+  @cont = false
 end
 
 while @cont
